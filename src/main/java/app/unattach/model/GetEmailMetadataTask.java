c95fed55415ce5091538bc4eba31144719b4ba8d
Lapo Luchini <lapo@lapo.it>
2024-08-24 14:12:24 +0200

    Fix Gmail speed limits again. (even tough 40 should be the correct batch size from documentation)

--- src/main/java/app/unattach/model/GetEmailMetadataTask.java
+++ src/main/java/app/unattach/model/GetEmailMetadataTask.java
@@ -23,7 +23,8 @@ public class GetEmailMetadataTask implements LongTask<GetEmailMetadataTask.Resul
   // https://developers.google.com/gmail/api/reference/quota
   // https://console.cloud.google.com/apis/api/gmail.googleapis.com/quotas?project=walsh-unattach-gmail
   // https://console.cloud.google.com/iam-admin/quotas?project=walsh-unattach-gmail&pageState=(%22allQuotasTable%22:(%22s%22:%5B(%22i%22:%22currentUsage%22,%22s%22:%220%22),(%22i%22:%22currentPercent%22,%22s%22:%220%22),(%22i%22:%22sevenDayPeakPercent%22,%22s%22:%220%22),(%22i%22:%22sevenDayPeakUsage%22,%22s%22:%220%22),(%22i%22:%22serviceTitle%22,%22s%22:%220%22),(%22i%22:%22displayName%22,%22s%22:%220%22),(%22i%22:%22displayDimensions%22,%22s%22:%220%22)%5D,%22f%22:%22%255B%255D%22))
-  private final int batchSize = 25;
+  private final int batchSize = 20;
+  private final int batchSleep = 1_000;
   private final int numberOfBatches;
   private final Worker worker;
   private int currentBatchNumber;
@@ -49,7 +50,7 @@ public class GetEmailMetadataTask implements LongTask<GetEmailMetadataTask.Resul
   public Result takeStep() throws LongTaskException {
     try {
       if (currentBatchNumber != 0) {
-        clock.sleep(10_000);
+        clock.sleep(batchSleep);
       }
       final int startIndexInclusive = currentBatchNumber * batchSize;
       final int endIndexExclusive = Math.min(emailIds.size(), (currentBatchNumber + 1) * batchSize);
