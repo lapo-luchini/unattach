c7a16451a144b18e5872ec43ceaadc43a298e80a
Lapo Luchini <lapo@lapo.it>
2025-03-25 19:52:44 +0100

    Add parameters for batch size and delay.

--- src/main/java/app/unattach/model/GetEmailMetadataTask.java
+++ src/main/java/app/unattach/model/GetEmailMetadataTask.java
@@ -23,8 +23,8 @@ public class GetEmailMetadataTask implements LongTask<GetEmailMetadataTask.Resul
   // https://developers.google.com/gmail/api/reference/quota
   // https://console.cloud.google.com/apis/api/gmail.googleapis.com/quotas?project=walsh-unattach-gmail
   // https://console.cloud.google.com/iam-admin/quotas?project=walsh-unattach-gmail&pageState=(%22allQuotasTable%22:(%22s%22:%5B(%22i%22:%22currentUsage%22,%22s%22:%220%22),(%22i%22:%22currentPercent%22,%22s%22:%220%22),(%22i%22:%22sevenDayPeakPercent%22,%22s%22:%220%22),(%22i%22:%22sevenDayPeakUsage%22,%22s%22:%220%22),(%22i%22:%22serviceTitle%22,%22s%22:%220%22),(%22i%22:%22displayName%22,%22s%22:%220%22),(%22i%22:%22displayDimensions%22,%22s%22:%220%22)%5D,%22f%22:%22%255B%255D%22))
-  private final int batchSize = 20;
-  private final int batchSleep = 1_000;
+  private final int batchSize = Integer.parseInt(System.getProperty("app.unattach.gmail.batchSize", "20"));
+  private final int batchSleep = Integer.parseInt(System.getProperty("app.unattach.gmail.batchSleep", "10000"));
   private final int numberOfBatches;
   private final Worker worker;
   private int currentBatchNumber;
